412a332b6c8b64c1e62d1644c227c25078f995d7
lz1998 <875543533@qq.com>
2022-11-12 22:25:42 +0800

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Default::default();
+        }
+        let mut parts = s.split(",");
+        if parts.clone().count() != 2 {
+            return Default::default();
+        }
+        let name = parts.next().unwrap();
+        if name.is_empty() {
+            return Default::default();
+        }
+        if let Some(Ok(age)) = parts.next().map(|s| s.parse()) {
+            Person {
+                name: name.to_string(),
+                age,
+            }
+        } else {
+            Default::default()
+        }
     }
 }
 
